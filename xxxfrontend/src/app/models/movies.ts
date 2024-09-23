df724b4222e0b989a005a656f11554804e8d468b
Jannick Mørup <jannickmorup@gmail.com>
2024-09-23 11:01:15 +0200

    added class

---
+++ xxxfrontend/src/app/models/movies.ts
@@ -0,0 +1,5 @@
+export class Movie {
+    id: number=0;
+    title: string="";
+    duration: number=0;
+}
---
+++ xxxfrontend/src/app/models/review.ts
@@ -0,0 +1,5 @@
+export class Review {
+    id: number=0;
+    rading: number=0;
+    comment: string="";
+}
