bb7658d5d87a0f6f9deda754bed621443b37b557
Jannick Mørup <jannickmorup@gmail.com>
2024-09-25 21:43:53 +0200

    jj

--- xxxfrontend/src/app/app.component.ts
+++ xxxfrontend/src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component,OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-<<<<<<< HEAD
 import { CommonModule } from '@angular/common';
 import { HttpClientModule } from '@angular/common/http';
 import { Director } from './models/directors';
@@ -11,10 +10,6 @@ import { directorservice } from './service/directors.service';
 import { MovieService } from './service/movie.service';
 import { ReviewService } from './service/review.service';
 import { GenreService } from './service/genre.service';
-=======
-import { Director } from './models/directors';
-import { directorservice } from './service/directors.service';
->>>>>>> 8feadc6d7ee1ba8218279898d22f38bd4d7376ee
 
 @Component({
   selector: 'app-root',
@@ -30,7 +25,7 @@ import { directorservice } from './service/directors.service';
 })
 export class AppComponent {
   title = 'xxxfrontend';
-<<<<<<< HEAD
+
   
   directors: Director[] = [];
   genres: Genre[] = [];
@@ -64,6 +59,4 @@ export class AppComponent {
   getAllReviews(): void {
     this.reviewService.getallReviews().subscribe(reviews => this.reviews = reviews);
   }
-=======
->>>>>>> 8feadc6d7ee1ba8218279898d22f38bd4d7376ee
 }
