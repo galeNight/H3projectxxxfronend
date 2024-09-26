f62ff3dfa68aeef4eac476d8ce1b36e23f1d5661
jannick <jannickmorup@gmail.com>
2024-09-26 08:54:46 +0200

    ..

--- xxxfrontend/src/app/app.routes.ts
+++ xxxfrontend/src/app/app.routes.ts
@@ -1,5 +1,21 @@
+import { NgModule } from '@angular/core';
 import { Routes } from '@angular/router';
+import { RouterModule, Route } from '@angular/router';
+import { DirectorsComponent } from './components/directors/directors.component';
+import { GenreComponent } from './components/genre/genre.component';
+import { MovieComponent } from './components/movie/movie.component';
+import { ReviewComponent } from './components/review/review.component';
 
 export const routes: Routes = [
-    
+    {path: 'directors', component: DirectorsComponent},
+    {path: 'genre', component: GenreComponent},
+    {path: 'movie', component: MovieComponent},
+    {path: 'review', component: ReviewComponent}
 ];
+
+@NgModule({
+    imports: [RouterModule.forRoot(routes)],
+    exports: [RouterModule]
+})
+
+export class AppRoutingModule{}
